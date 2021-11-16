8e73da6736121c51cf1f5fd8835b895391171555
fmtacuri <freddyTO95>
2021-11-15 21:33:03 -0500

    [BCE-feature] Implementacion de servicio poke con exChange

---
+++ tacuri-test/src/main/java/com/pichincha/tacuri/ln/dto/ResponseDto.java
@@ -0,0 +1,10 @@
+package com.pichincha.tacuri.ln.dto;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class ResponseDto {
+  String message;
+}
---
+++ tacuri-test/src/main/java/com/pichincha/tacuri/ln/service/exchange/PokeService.java
@@ -0,0 +1,7 @@
+package com.pichincha.tacuri.ln.service.exchange;
+
+import com.pichincha.tacuri.ln.dto.ResponseDto;
+
+public interface PokeService {
+    ResponseDto getPokeResponse();
+}
